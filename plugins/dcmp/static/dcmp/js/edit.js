0450e4afc740d743fe9e5217b3b09fabb983b580
liaozhiyue <liaozhiyue@gotokeep.com>
2018-10-16 19:04:14 +0800

    NamedHivePartitionSensor support

--- plugins/dcmp/static/dcmp/js/edit.js
+++ plugins/dcmp/static/dcmp/js/edit.js
@@ -132,7 +132,7 @@ Date.prototype.Format = function (fmt) { //author: meizz
                     field_html.push(get_ace_script(task_type, "python"));
                 }else if(task_type == "hive_sensor"){
                     field_html.push('<textarea ' + (readonly? ' readonly="readonly" ': '') + ' id="ace_' + task_id + '_' + task_type + '" class="form-control" rows="1" name="command">' + (task_type == task[field_name]? task["command"]: '') + '</textarea>');
-                    field_html.push(get_ace_script(task_type, "python", 1));
+                    field_html.push(get_ace_script(task_type, "text", 1));
                 }
                 else if(task_type == "timedelta_sensor"){
                     field_html.push('<textarea ' + (readonly? ' readonly="readonly" ': '') + ' id="ace_' + task_id + '_' + task_type + '" class="form-control" rows="1" name="command">' + (task_type == task[field_name]? task["command"]: '') + '</textarea>');
